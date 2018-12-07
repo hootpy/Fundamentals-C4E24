572e19bf1f2ae78444173656471fece9b4776270
Hien Pham <45201848+hootpy@users.noreply.github.com>
2018-12-07 16:19:55 +0700

    Update ex3.py

--- session5/homework/ex3.py
+++ session5/homework/ex3.py
@@ -1,15 +1,34 @@
 #Implement quiz program
 
+question = [
+    "If x = 8, then what is the value of 4(x+3)"
+]
 
-question = {
-    "question": 'If x = 8 , then what is the value of 4(x+3)',
-    "1.": 35,
-    "2.": 36,
-    "3.": 40,
-    "4.": 44,
-}
+
+choice = [
+    {
+        "1":35,
+        "2":36,
+        "3":40,
+        "4":44
+    }
+]
+
+answer = [
+    "4"
+]
 
 
 print('Hello to quiz')
 print('Answer the following question:')
 
+print(question[0])
+for k,v in choice[0].items():
+    print(k,v,sep=":")
+
+ans = input("Your answer is: ")
+
+if ans == answer[0]:
+    print("Correct!")
+else:
+    print("Incorrect!")
