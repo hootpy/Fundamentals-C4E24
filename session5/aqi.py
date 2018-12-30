8e387dd2e46f66fd64d4b35eb81eab63b4eb1adf
Phạm Hiển <phamhien@Phams-MacBook-Pro.local>
2018-12-30 22:27:53 +0700

    update

--- session5/aqi.py
+++ session5/aqi.py
@@ -1,22 +1,32 @@
 from urllib.request import urlopen
 import json
 
-url = "https://store.steampowered.com/api/appdetails?appids=730&filters=price_overview&cc=vn"
+while True:
+    id_game= str(input("Enter game id:"))
+    if id_game.lower() == "exit":
+        break
+    elif id_game.isdigit() == False:
+        print("Enter game id!")
+    else:
+        url = f"https://store.steampowered.com/api/appdetails?appids={id_game}&cc=vn"
 
+        #1. Open connection
+        conn = urlopen(url)
 
-#1. Open connection
-conn = urlopen(url)
+        #2. Read data
+        raw_data = conn.read()
 
-#2. Read data
-raw_data = conn.read()
+        #3. Decode data
+        text = raw_data.decode("utf-8")
+        data = json.loads(text)
 
-#3. Decode data
-text = raw_data.decode("utf-8")
-data = json.loads(text)
-
-# results = data["results"]
-# for i in results:
-#     print("City:",i["s"]["n"][0])
-#     print("Index:",i["s"]["a"])
-#     print("******************************")
-print(data)
+        if data[id_game]["success"]:
+            sub_info = data[id_game]["data"]["package_groups"][0]["subs"]
+            for i in sub_info:
+                game_name = i["option_text"]
+                price = (int(i["price_in_cents_with_discount"])/100) * 0.78
+                print(game_name)
+                print("Gia ban", price)
+                print("**********************")
+        else:
+            print("Sai ma game")
